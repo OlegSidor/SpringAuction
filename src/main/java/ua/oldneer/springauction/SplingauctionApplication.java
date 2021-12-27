0ac45b3bf852f7e07a77df8d73b01cb18ef16733
oldneer <oldeer.dev@gmail.com>
2021-12-27 20:02:45 +0200

    initial commit

---
+++ src/main/java/ua/oldneer/springauction/SplingauctionApplication.java
@@ -0,0 +1,14 @@
+package ua.oldneer.springauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SplingauctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplingauctionApplication.class, args);
+    }
+
+}
